f7f57bdf5473c01d3673e34e2dbebfc513128e6f
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-12-11 11:37:23 +0700

    Add user keuangan models

---
+++ keuangan/lib/models/user_keuanganadmin.dart
@@ -0,0 +1,53 @@
+// To parse this JSON data, do
+//
+//     final keuanganAdmin = keuanganAdminFromJson(jsonString);
+
+import 'dart:convert';
+
+List<KeuanganAdmin> keuanganAdminFromJson(String str) => List<KeuanganAdmin>.from(json.decode(str).map((x) => KeuanganAdmin.fromJson(x)));
+
+String keuanganAdminToJson(List<KeuanganAdmin> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class KeuanganAdmin {
+    KeuanganAdmin({
+        required this.model,
+        required this.pk,
+        required this.fields,
+    });
+
+    String model;
+    int pk;
+    Fields fields;
+
+    factory KeuanganAdmin.fromJson(Map<String, dynamic> json) => KeuanganAdmin(
+        model: json["model"],
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "model": model,
+        "pk": pk,
+        "fields": fields.toJson(),
+    };
+}
+
+class Fields {
+    Fields({
+        required this.uangUser,
+        required this.user,
+    });
+
+    double uangUser;
+    int user;
+
+    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        uangUser: json["uang_user"].toDouble(),
+        user: json["user"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "uang_user": uangUser,
+        "user": user,
+    };
+}
