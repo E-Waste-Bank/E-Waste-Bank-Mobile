d98f56234a7a3affaa91564567dfacb0510b1ee6
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-12-11 16:45:01 +0700

    Remove fetchCashouts from cashout model
    We will be using CookieRequest to make authenticated requests, so we need to do it in the widget itself.

--- keuangan/lib/models/user_cashout.dart
+++ keuangan/lib/models/user_cashout.dart
@@ -2,7 +2,6 @@
 //
 //     final cashouts = cashoutsFromJson(jsonString);
 
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 List<Cashouts> cashoutsFromJson(String str) =>
@@ -84,23 +83,3 @@ class EnumValues<T> {
   }
 }
 
-// Buat fetch
-Future<List<Cashouts>> fetchCashouts(String urlString) async {
-  var uri = Uri.parse(urlString);
-
-  var response = await http.get(uri, headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-  });
-
-  var data = jsonDecode(utf8.decode(response.bodyBytes));
-
-  List<Cashouts> listCashouts = [];
-  for (var each in data) {
-    if (each != null) {
-      listCashouts.add(Cashouts.fromJson(each));
-    }
-  }
-
-  return listCashouts;
-}
