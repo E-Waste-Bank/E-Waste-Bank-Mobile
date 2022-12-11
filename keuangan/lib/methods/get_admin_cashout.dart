4a83d4950d06ddd68d34dfa586e4525592836d9d
raditya.aditama <raditya.aditama@ui.ac.id>
2022-12-11 16:08:07 +0700

    Add admin http.get utils

---
+++ keuangan/lib/methods/get_admin_cashout.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:keuangan/models/admin_cashout_model.dart';
+
+Future<List<Cashout>> fetchToDo() async {
+  var url = Uri.parse('https://e-waste-bank.up.railway.app/keuangan/json/admin-cashouts/');
+  var response = await http.get(
+    url,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  );
+
+  // melakukan decode response menjadi bentuk json
+  var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+  // melakukan konversi data json menjadi object Cashout
+  List<Cashout> listCashout = [];
+  for (var d in data) {
+    if (d != null) {
+      listCashout.add(Cashout.fromJson(d));
+    }
+  }
+
+  return listCashout;
+}
---
+++ keuangan/lib/methods/get_admin_keuangan.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:keuangan/models/admin_keuanganadmin_model.dart';
+
+Future<List<KeuanganAdmin>> fetchToDo() async {
+  var url = Uri.parse('https://e-waste-bank.up.railway.app/keuangan/json/admin/');
+  var response = await http.get(
+    url,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  );
+
+  // melakukan decode response menjadi bentuk json
+  var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+  // melakukan konversi data json menjadi object KeuanganAdmin
+  List<KeuanganAdmin> listKeuanganAdmin = [];
+  for (var d in data) {
+    if (d != null) {
+      listKeuanganAdmin.add(KeuanganAdmin.fromJson(d));
+    }
+  }
+
+  return listKeuanganAdmin;
+}
