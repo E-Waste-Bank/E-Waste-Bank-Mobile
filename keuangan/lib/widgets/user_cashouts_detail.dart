be1656bf432006a82aef90b4aed6f74d77f65468
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-12-11 16:45:21 +0700

    Initial implementation of user cashout view pages

---
+++ keuangan/lib/widgets/user_cashouts_detail.dart
@@ -0,0 +1,95 @@
+// TODO import 'package:e_waste_bank_mobile/widget/drawer.dart';
+import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+
+import 'package:keuangan/models/user_cashout.dart';
+import 'package:e_waste_bank_mobile/drawer.dart';
+
+class CashoutDetailPage extends StatelessWidget {
+  const CashoutDetailPage({super.key, required this.cashouts});
+
+  final Cashouts cashouts;
+
+  @override
+  Widget build(BuildContext context) {
+    const double largeHeight = 20;
+    const double smallHeight = 4;
+
+    // final DateFormat dateFormat = DateFormat('LLL dd, yyyy');
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Detail'),
+        ),
+        drawer: const MyDrawer(), // TODO add to drawer
+        body: Container(
+          margin: const EdgeInsets.all(7.5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  "Penarikan (ID: ${cashouts.pk}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: largeHeight),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text("Amount: ",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Expanded(
+                    child: Text(
+                      "${cashouts.fields.amount}",
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: smallHeight),
+              const SizedBox(height: smallHeight),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text("Disetujui: ",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Expanded(
+                    child: Text(
+                      cashouts.fields.approved ? "Ya" : "Tidak",
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+              const Spacer(),
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      ),
+                      child: const Text(
+                        'Back',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ));
+  }
+}
---
+++ keuangan/lib/widgets/user_list_cashouts.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:keuangan/models/user_cashout.dart';
+
+import 'package:keuangan/widgets/user_cashouts_detail.dart';
+import 'package:e_waste_bank_mobile/drawer.dart';
+
+class UserListCashoutsPage extends StatefulWidget {
+  const UserListCashoutsPage({Key? key}) : super(key: key);
+
+  @override
+  State<UserListCashoutsPage> createState() => _UserListCashoutsPageState();
+}
+
+class _UserListCashoutsPageState extends State<UserListCashoutsPage> {
+  late Future<List<Cashouts>> fetchedCashouts;
+
+  Future<List<Cashouts>> fetchCashouts() async {
+    final request = context.watch<CookieRequest>();
+    var data = await request
+        .get("http://localhost:8000/keuangan/json/user-cashouts-api/");
+
+    List<Cashouts> listCashouts = [];
+    for (var each in data) {
+      if (each != null) {
+        listCashouts.add(Cashouts.fromJson(each));
+      }
+    }
+
+    return listCashouts;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchedCashouts = fetchCashouts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My Cashouts'),
+        ),
+        drawer: const MyDrawer(),
+        body: FutureBuilder(
+          future: fetchedCashouts,
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (!snapshot.hasData) {
+              return Column(
+                children: const [
+                  Text(
+                    "Belum ada penarikan yang anda buat.",
+                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) {
+                          return CashoutDetailPage(
+                              cashouts: snapshot.data![index]);
+                        },
+                      ));
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 10),
+                      padding: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15.0),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 5)
+                          ]),
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Text(
+                                  "Penarikan ID: ${snapshot.data![index].pk}",
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Text(
+                                  "Nominal: Rp.${snapshot.data![index].fields.amount}",
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        )
+      );
+  }
+}
