48ae576f29156e7e36c21a0f4cf705e9ed6b97d4
raditya.aditama <raditya.aditama@ui.ac.id>
2022-12-12 22:14:42 +0700

    Remove unused code

--- keuangan/lib/methods/get_admin_keuangan.dart
+++ keuangan/lib/methods/get_admin_keuangan.dart
@@ -4,19 +4,9 @@ import 'package:keuangan/models/admin_keuanganadmin_model.dart';
 
 Future<List<KeuanganAdmin>> fetchKeuanganAdmin(context) async {
   final requester = Provider.of<CookieRequest>(context, listen: false);
-  // var url = Uri.parse('https://127.0.0.1:8000/keuangan/json/admin/');
-  // var url = Uri.parse('https://e-waste-bank.up.railway.app/keuangan/json/admin/');
 
   var data = await requester
       .get("https://e-waste-bank.up.railway.app/keuangan/json/admin/");
-
-  // var response = await http.get(
-  //   url,
-  //   headers: {
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Content-Type": "application/json",
-  //   },
-  // );
 
   List<KeuanganAdmin> listKeuanganAdmin = [];
   for (var each in data) {
